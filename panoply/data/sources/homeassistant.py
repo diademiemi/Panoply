3e838dd5cd2f4700882c79a5c4f0d5ceb5f563c2
diademiemi <emilia@diademiemi.me>
2022-04-30 20:54:45 +0200

    Catch errors

--- panoply/data/sources/homeassistant.py
+++ panoply/data/sources/homeassistant.py
@@ -10,13 +10,16 @@ def getState(entity):
     }
 
     endpoint = "{url}/api/states/{entity}".format(url = config.HOME_ASSISTANT_URL, entity = entity)
+    
+    try:
+        response = get(endpoint, headers=headers)
 
-    response = get(endpoint, headers=headers)
-
-    if response.status_code == 200:
-        data = json.loads(response.content.decode('utf-8'))
-        return data["state"]
-    else:
+        if response.status_code == 200:
+            data = json.loads(response.content.decode('utf-8'))
+            return data["state"]
+        else:
+            return "ERROR"
+    except:
         return "ERROR"
         
 def getColor(entity):
@@ -26,14 +29,17 @@ def getColor(entity):
     }
 
     endpoint = "{url}/api/states/{entity}".format(url = config.HOME_ASSISTANT_URL, entity = entity)
+    
+    try:
+        response = get(endpoint, headers=headers)
 
-    response = get(endpoint, headers=headers)
-
-    if response.status_code == 200:
-        data = json.loads(response.content.decode('utf-8'))
-        if "rgb_color" in data["attributes"]:
-            return data["attributes"]["rgb_color"]
+        if response.status_code == 200:
+            data = json.loads(response.content.decode('utf-8'))
+            if "rgb_color" in data["attributes"]:
+                return data["attributes"]["rgb_color"]
+            else:
+                return (0, 0, 0)
         else:
             return (0, 0, 0)
-    else:
+    except:
         return (0, 0, 0)
